52722dad4a1b4927e32a0e7965bc9312dfd07e7e
Ruyi <ruyi@kooboo.cn>
2023-05-06 14:08:19 +0800

    [feat]: add TimeMonitorAttribute

---
+++ EasyTool.AspectCoreExtension/Attributes/TimeMonitorAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using AspectCore.DynamicProxy;
+
+namespace EasyTool.AspectCoreExtension.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TimeMonitorAttribute : AbstractInterceptorAttribute
+    {
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            await next(context);
+            stopWatch.Stop();
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Method: {context.ProxyMethod.Name}, cast: {stopWatch.ElapsedMilliseconds} ms...");
+            Console.ForegroundColor = color;
+        }
+    }
+}
---
+++ EasyTool.AspectCoreExtension/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using AspectCore.Configuration;
+using AspectCore.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyTool.AspectCoreExtension.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void UseProxyServiceProvider(this IServiceCollection services)
+        {
+            var proxyServiceProvider = services.BuildDynamicProxyProvider();
+            services.AddSingleton<IServiceProvider>(proxyServiceProvider);
+        }
+
+        public static void UseInterceptor(this IServiceCollection services, Type attributeType)
+        {
+            services.ConfigureDynamicProxy(config => config.Interceptors.AddTyped(attributeType));
+        }
+    }
+}
